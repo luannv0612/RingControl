f1d6a6c57217e079b84768696559add2cf2cc754
NVLuan <luandcnd@gmail.com>
2017-08-18 10:46:44 +0700

    RingControl

---
+++ RingControl/RingControl.h
@@ -0,0 +1,19 @@
+//
+//  RingControl.h
+//  RingControl
+//
+//  Created by CMC iOS Dev on 18/08/2017.
+//  Copyright © 2017 CMC Dev iOS Luannv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RingControl.
+FOUNDATION_EXPORT double RingControlVersionNumber;
+
+//! Project version string for RingControl.
+FOUNDATION_EXPORT const unsigned char RingControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RingControl/PublicHeader.h>
+
+
---
+++ RingControlTests/RingControlTests.swift
@@ -0,0 +1,36 @@
+//
+//  RingControlTests.swift
+//  RingControlTests
+//
+//  Created by CMC iOS Dev on 18/08/2017.
+//  Copyright © 2017 CMC Dev iOS Luannv. All rights reserved.
+//
+
+import XCTest
+@testable import RingControl
+
+class RingControlTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
